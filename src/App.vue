49eeba94044bb6875d667f071f750f12d3ffe1ee
andrei-deeyu <thedeeyu@gmail.com>
2025-01-29 16:41:30 +0200

    change navbar to bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,12 @@ import Dashboard from './pages/Dashboard.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="AStatus logo" class="logo" src="./assets/logo.svg" width="300" height="60" />
-    <!--
-      <div id="nav-item">
-
-      </div>
-    -->
+  <header class="navbar bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img alt="AStatus logo" src="./assets/logo.png" />
+      </a>
+    </div>
   </header>
 
   <main>
@@ -18,17 +17,5 @@ import Dashboard from './pages/Dashboard.vue'
 </template>
 
 <style scoped>
-header {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 60px;
-  line-height: 1.5;
-  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.25);
-}
 
-.logo {
-  margin-left: 1rem;
-}
 </style>
